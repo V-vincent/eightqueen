02632638f817e10bfbacfa9d9cb2b8472a5d2d51
pengjunqiang <pengjunqiang@foresee.com.cn>
2021-02-05 21:26:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <eight-queen />
+  </div>
+</template>
+
+<script>
+import EightQueen from './components/EightQueen'
+
+export default {
+  name: 'App',
+  components: {
+    EightQueen
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EightQueen.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="title">八皇后问题</div>
+        <div class="gird" v-for="(row, r_index) in grids" :key="r_index">
+            <div class="cell" v-for="cell in row" :key="cell.key">
+                <div v-if="cell.ok">Q</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const grids = new Array(8).fill(1).map((_, r) => {
+    return new Array(8).fill(1).map((_, c) => {
+        return {
+            key: `key-${r * 8 + c}`,
+            ok: false,
+        }
+    })
+})
+export default {
+    data() {
+        return {
+            grids
+        }
+    },
+    methods: {
+        change() {
+            console.log(2);
+            console.log(1);
+        }
+    },
+    // render() {
+    //     return (
+    //         <div class="grid">
+    //             {this.grids.map((row, r_index) => (
+    //                 <div class="row" key={r_index}>
+    //                     {row.map(item => (
+    //                         <div class="cell" key={item.key}>
+    //                             {item.ok ? <div>Q</div> : ""}
+    //                         </div>
+    //                     ))}
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     )
+    // }
+}
+</script>
